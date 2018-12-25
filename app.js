cc678bddce127ad0d0b8565eb93c0ff563cb7eec
RachelPfeiffer <42552533+RachelPfeiffer@users.noreply.github.com>
2018-12-25 20:11:35 +0200

    added actual projects

---
+++ app.js
@@ -0,0 +1,15 @@
+const fillProjectSection = function(projects) {
+  const projectSection = document.querySelector('.projects');
+  for (project of projects) {
+    const newProject = document.createElement('div');
+    newProject.className = 'project';
+    newProject.innerHTML = '<img src="images/'+project.id+'.jpg" class="proj-pic" width="100%" alt="'+project.name+'"><h1 class="project-name">'+project.name+'</h1><div class="view"><a class="github-link" href="'+project.github+'" target="_blank">GitHub Repo</a><a href="'+project.demo+'" class="demo-link" target="_blank">Live Demo</a>';
+    projectSection.appendChild(newProject);
+  }
+}
+
+fetch("projects.json").then(function (response) {
+return response.json();
+}).then(function (response) {
+  fillProjectSection(response.projects);
+});
