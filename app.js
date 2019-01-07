4fd64cbc4105267fea7849f215a1d8f03784c09d
Rachel Pfeiffer <rochelfried9@gmail.com>
2019-01-07 08:22:22 +0200

    fixing paths

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const fillProjectSection = function(projects) {
   for (project of projects) {
     const newProject = document.createElement('div');
     newProject.className = 'project';
-    newProject.innerHTML = '<img src="./images/'+project.id+'.jpg" class="proj-pic" width="100%" alt="'+project.name+'"><h1 class="project-name">'+project.name+'</h1><div class="view"><a class="github-link" href="'+project.github+'" target="_blank">GitHub Repo</a><a href="'+project.demo+'" class="demo-link" target="_blank">Live Demo</a>';
+    newProject.innerHTML = '<img src="./images/0.jpg" class="proj-pic" width="100%" alt="'+project.name+'"><h1 class="project-name">'+project.name+'</h1><div class="view"><a class="github-link" href="'+project.github+'" target="_blank">GitHub Repo</a><a href="'+project.demo+'" class="demo-link" target="_blank">Live Demo</a>';
     projectSection.appendChild(newProject);
   }
 }
