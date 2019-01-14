7c3f0479ac8418715a4ffcc9d8fcd6379980a5a7
RachelPfeiffer <42552533+RachelPfeiffer@users.noreply.github.com>
2019-01-14 12:33:48 +0200

    switching to no-scroll design

--- app.js
+++ app.js
@@ -1,15 +1,60 @@
-const fillProjectSection = function(projects) {
-  const projectSection = document.querySelector('.projects');
+console.log("script is linked and running");
+const navList = document.querySelector('ol');
+
+const fillNav = function(projects) {
   for (project of projects) {
-    const newProject = document.createElement('div');
-    newProject.className = 'project';
-    newProject.innerHTML = '<img src="https://rachelpfeiffer.github.io/Portfolio-Project/images/'+project.id+'.JPG" class="proj-pic" width="100%" alt="'+project.name+'"><h1 class="project-name">'+project.name+'</h1><div class="view"><a class="github-link" href="'+project.github+'" target="_blank">GitHub Repo</a><a href="'+project.demo+'" class="demo-link" target="_blank">Live Demo</a>';
-    projectSection.appendChild(newProject);
+    const projListing = document.createElement('li');
+    projListing.innerHTML = '<h1>0'+project.id+'</h1><h4>'+project.name+'</h4>';
+    navList.appendChild(projListing);
   }
+
 }
 
-fetch("https://rachelpfeiffer.github.io/Portfolio-Project/projects.json").then(function (response) {
-return response.json();
-}).then(function (response) {
-  fillProjectSection(response.projects);
+const fillProjectSection = function(project) {
+  console.log(project);
+//   const main = document.querySelector('main');
+//   const projSection = document.createElement('div');
+const pic = document.querySelector('.proj-pic');
+pic.src = '/images/'+project.id+'.JPG';
+
+const name = document.querySelector('.proj-name');
+name.innerText = project.name;
+
+const description = document.querySelector('.description');
+description.innerText = project.description;
+
+const github = document.querySelector('.gitHub');
+github.href = project.github;
+
+const demo = document.querySelector('.live');
+demo.href = project.demo;
+}
+//
+//   const infoSpot = document.querySelector('.proj-info-spot');
+//
+//   const infoDiv = document.querySelector('.proj-info');
+//
+//   const name = document.querySelector('proj-name');
+//       name.innerText = project.name;
+//
+//
+//       const view = document.querySelector('view');
+//
+//       const gitHub = document.querySelector('.gitHub');
+//       gitHub.href = project.github;
+//       gitHub.target = '_blank';
+//
+//       const demo = document.querySelector('live');
+//       demo.href = project.demo;
+//
+//       const describe = document.querySelector('description');
+//       describe.innerText = project.description;
+// }
+
+let response;
+fetch("projects.json").then(function (info) {
+return info.json();
+}).then(function (info) {
+  fillProjectSection(info.projects[1]);
+  fillNav(info.projects);
 });
